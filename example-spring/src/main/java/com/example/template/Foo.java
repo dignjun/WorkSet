31ae6d870360017b657b2f9cd033173fae272f24
鼬 <dingjun941035104@163.com>
2019-03-01 14:32:04 +0800

    装饰者接口

--- example-spring/src/main/java/com/example/template/Foo.java
+++ example-spring/src/main/java/com/example/template/Foo.java
@@ -1,45 +1,37 @@
 package com.example.template;
 
-import org.springframework.dao.DataAccessException;
-import org.springframework.jdbc.core.ConnectionCallback;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.support.JdbcUtils;
-
-import java.sql.*;
-import java.util.HashMap;
-
 public class Foo {
     public static void main(String[] args) {
-        JdbcTemplate jdbcTemplate = new JdbcTemplate();
-        jdbcTemplate.execute(new ConnectionCallback() {
-
-            public Object doInConnection(Connection con) throws SQLException, DataAccessException {
-                // do insert
-                PreparedStatement ps = null;
-                try {
-                    ps = con.prepareStatement("insert into foo (name,age) values (zhangsan, 18)");
-                    ps.executeUpdate();
-                } finally {
-                    JdbcUtils.closeStatement(ps);
-                }
-                // get the key
-                Statement keystmt = null;
-                ResultSet rs = null;
-                HashMap keys = new HashMap(1);
-                try {
-                    keystmt = con.createStatement();
-                    rs = keystmt.executeQuery("select * from foo");
-                    if(rs.next()){
-                        String name = rs.getString(1);
-                        int age = rs.getInt(2);
-                        System.out.println(name + ":" + age);
-                    }
-                } finally {
-                    JdbcUtils.closeResultSet(rs);
-                    JdbcUtils.closeStatement(keystmt);
-                }
-                return null;
-            }
-        });
+//        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+//        jdbcTemplate.execute(new ConnectionCallback() {
+//
+//            public Object doInConnection(Connection con) throws SQLException, DataAccessException {
+//                // do insert
+//                PreparedStatement ps = null;
+//                try {
+//                    ps = con.prepareStatement("insert into foo (name,age) values (zhangsan, 18)");
+//                    ps.executeUpdate();
+//                } finally {
+//                    JdbcUtils.closeStatement(ps);
+//                }
+//                // get the key
+//                Statement keystmt = null;
+//                ResultSet rs = null;
+//                HashMap keys = new HashMap(1);
+//                try {
+//                    keystmt = con.createStatement();
+//                    rs = keystmt.executeQuery("select * from foo");
+//                    if(rs.next()){
+//                        String name = rs.getString(1);
+//                        int age = rs.getInt(2);
+//                        System.out.println(name + ":" + age);
+//                    }
+//                } finally {
+//                    JdbcUtils.closeResultSet(rs);
+//                    JdbcUtils.closeStatement(keystmt);
+//                }
+//                return null;
+//            }
+//        });
     }
 }
